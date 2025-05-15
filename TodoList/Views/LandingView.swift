b04344e9e1b83731a7a94c1cdba020fbe3bcdb37
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-05-15 14:28:11 -0400

    Ensure that you cannot input a to-do item that is blank.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -59,6 +59,7 @@ struct LandingView: View {
                         newItemDescription = ""
                     }
                     .font(.caption)
+                    .disabled(newItemDescription.trimmingCharacters(in: .whitespaces).isEmpty == true)
                 }
                 .padding(20)
                 
