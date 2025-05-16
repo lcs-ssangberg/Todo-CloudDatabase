2c379426c78a6a479017da9f99de33dbf518f221
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-05-16 11:31:47 -0400

    Used ContentUnavailableView to provide a prompt when no to-do items exist.
    also first push as was offline for the last few commits, updated the database to actually allow anon signins and such, user differentiation

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -26,27 +26,39 @@ struct LandingView: View {
             
             VStack {
                 
-                List($viewModel.todos) { $todo in
+                if viewModel.todos.isEmpty {
                     
-                    ItemView(currentItem: $todo)
-                    // Delete item
-                        .swipeActions {
-                            Button(
-                                "Delete",
-                                role: .destructive,
-                                action: {
-                                    viewModel.delete(todo)
-                                }
-                            )
-                        }
+                    //show the prompt to add a new to-do item
+                    ContentUnavailableView(
+                        "No to-do items",
+                        systemImage: "pencil.tip.crop.circle.badge.plus",
+                        description: Text("Add a reminder to get started")
+                    )
                     
-                }
-                .searchable(text: $searchText)
-                .onChange(of: searchText) {
-                    Task {
-                        try await viewModel.filterTodos(on: searchText)
+                } else {
+                    
+                    //show list of items
+                    List($viewModel.todos) { $todo in
+                        
+                    
+                        ItemView(currentItem: $todo)
+                        // Delete item
+                            .swipeActions {
+                                Button(
+                                    "Delete",
+                                    role: .destructive,
+                                    action: {
+                                        viewModel.delete(todo)
+                                    }
+                                )
+                            }
+                        
                     }
                 }
+                
+                
+                
+                
                 
                 
                 
@@ -67,6 +79,13 @@ struct LandingView: View {
                     } label: {
                         Image(systemName: "plus")
                     }
+                }
+            }
+            //handle searching in the list
+            .searchable(text: $searchText)
+            .onChange(of: searchText) {
+                Task {
+                    try await viewModel.filterTodos(on: searchText)
                 }
             }
             
